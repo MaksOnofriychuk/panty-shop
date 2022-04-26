4d07f414ee40cd54ca29a6ef2ba51addce5c77ad
Maks <maks@MacBook-Pro-Maks.local>
2022-04-26 23:12:57 +0300

    d/p

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route } from "react-router-dom";
 import Header from "./components/Header/header";
 import Home from "./pages/Home/home";
-import Panty  from "./pages/Panty/panty";
+import Panty from "./pages/Panty/panty";
 import Cart from "./pages/Cart/cart";
 import Ordering from "./pages/Ordering/ordering";
 
