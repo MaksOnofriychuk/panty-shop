13d03ff0d5f7ee8cf50b982273d36e69c0f5a539
Maks <maks@MacBook-Pro-Maks.local>
2022-08-01 10:15:51 +0300

    added/product

--- client/src/components/Promotion/promotion.jsx
+++ client/src/components/Promotion/promotion.jsx
@@ -4,7 +4,7 @@ import "./promotion.scss";
 const Promotion = () => {
   return (
     <div className="sale">
-      <h3 className="sale__title">Акция 7 трусиков за 229 грн</h3>
+      <h3 className="sale__title">Акция 7 трусиков за 250 грн</h3>
     </div>
   );
 };
